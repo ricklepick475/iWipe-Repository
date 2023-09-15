6935eff10b24ec1e34401543934f734147d0cbe7
Jacob Hanna <21013107@students.latrobe.edu.au>
2023-09-16 01:39:53 +1000

    1/4 Sprint 3 screens done, functionality to access said screen implemented

--- app/src/main/java/com/example/iwipe_repository/CreateUserPage.java
+++ app/src/main/java/com/example/iwipe_repository/CreateUserPage.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class CreateUserPage extends AppCompatActivity {
@@ -19,6 +20,15 @@ public class CreateUserPage extends AppCompatActivity {
             public void onClick(View view) {
                 Intent oldUser = new Intent(CreateUserPage.this, LoginUserPage.class);
                 startActivity(oldUser);
+            }
+        });
+
+        Button signUpButton = findViewById(R.id.signUpButton);
+        signUpButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent verifyIDInfoIntent = new Intent(CreateUserPage.this, VerifyIDInfo.class);
+                startActivity(verifyIDInfoIntent);
             }
         });
     }
