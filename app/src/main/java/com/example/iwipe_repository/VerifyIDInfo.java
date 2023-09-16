bcc998cfada301630a94497ffbba146de8c265f2
Jacob Hanna <21013107@students.latrobe.edu.au>
2023-09-16 14:45:10 +1000

    2/4 Screens in progress, no push yet

--- app/src/main/java/com/example/iwipe_repository/VerifyIDInfo.java
+++ app/src/main/java/com/example/iwipe_repository/VerifyIDInfo.java
@@ -1,19 +1,28 @@
 package com.example.iwipe_repository;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.widget.TextView;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
-
 public class VerifyIDInfo extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_verify_idinfo);
+
+        TextView missingID = findViewById(R.id.noID);
+
+        missingID.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent noID = new Intent(VerifyIDInfo.this, CreateUserPage.class);
+                startActivity(noID);
+            }
+        });
     }
 }
+
