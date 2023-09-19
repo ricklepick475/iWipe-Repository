f907f6faa4c895bde21b5ac2c8d8bfb8b0954c58
Jacob Hanna <21013107@students.latrobe.edu.au>
2023-09-19 19:45:04 +1000

    3/4 Screens completed

--- app/src/main/java/com/example/iwipe_repository/SelectID.java
+++ app/src/main/java/com/example/iwipe_repository/SelectID.java
@@ -2,7 +2,10 @@ package com.example.iwipe_repository;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 public class SelectID extends AppCompatActivity {
 
@@ -10,5 +13,32 @@ public class SelectID extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_select_id);
+
+        ImageButton driverID = findViewById(R.id.drivers);
+        ImageButton passportID = findViewById(R.id.passport);
+        ImageButton cardID = findViewById(R.id.card);
+        driverID.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent driverID = new Intent(SelectID.this, GetStarted.class);
+                startActivity(driverID);
+            }
+        });
+
+        passportID.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent passportID = new Intent(SelectID.this, GetStarted.class);
+                startActivity(passportID);
+            }
+        });
+
+        cardID.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent cardID = new Intent(SelectID.this, GetStarted.class);
+                startActivity(cardID);
+            }
+        });
     }
 }
