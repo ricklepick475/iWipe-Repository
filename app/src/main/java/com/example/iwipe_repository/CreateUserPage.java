2f29c2d2556df7d4439f98c8fcc25b00c48fe8a8
LatrobeJames <127180009+LatrobeJames@users.noreply.github.com>
2023-10-02 20:52:45 +1100

    Added FBHelper Added User Added tons of gradle dependencies modified createuserpage.java to register an account

--- app/src/main/java/com/example/iwipe_repository/CreateUserPage.java
+++ app/src/main/java/com/example/iwipe_repository/CreateUserPage.java
@@ -6,15 +6,27 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
-
+import android.widget.EditText;
 public class CreateUserPage extends AppCompatActivity {
+
+    private EditText firstNameEditText;
+    private EditText emailEditText;
+    private EditText passwordEditText;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_user_page);
 
+        // Initialize TextView
         TextView notNewToWipe = findViewById(R.id.notNewToiWipeText);
+
+        // Initialize EditText fields
+        firstNameEditText = findViewById(R.id.editTextTextPersonName);
+        emailEditText = findViewById(R.id.editTextTextPersonEmail);
+        passwordEditText = findViewById(R.id.editTextTextPassword);
+
         notNewToWipe.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -27,6 +39,15 @@ public class CreateUserPage extends AppCompatActivity {
         signUpButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                //Get registration Info
+                String firstName = firstNameEditText.getText().toString();
+                String email = emailEditText.getText().toString();
+                String password = passwordEditText.getText().toString();
+
+                FbHelper fbHelper = new FbHelper();
+                fbHelper.createUser(firstName, email, password);
+
+
                 Intent verifyIDInfoIntent = new Intent(CreateUserPage.this, VerifyIDInfo.class);
                 startActivity(verifyIDInfoIntent);
             }
